063fd9270dfda077356c5ba15e114b2a2f09e44e
kevinli <likevintw@gmail.com>
2021-12-16 11:34:41 +0800

    add get file adn direct paht, ok

--- test_utility.py
+++ test_utility.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.case import skip
 import utility
 
 # python3 -m unittest -v <.py>
@@ -93,8 +94,119 @@ class TestProcess(unittest.TestCase):
             result = utility.import_json(export_file_name[i])
             self.assertEqual(want[i], result)
 
-    def save_as_log(self):
+    def test_save_as_log(self):
         pass
+
+    def test_get_symbol_number_in_string(self):
+        string = []
+        symbol = []
+        want = []
+
+        # 1.
+        string.append("123/234/134/134/2134")
+        symbol.append("/")
+        want.append(4)
+        # 2.
+        string.append("123/234/134/134/2134")
+        symbol.append("1")
+        want.append(4)
+        # 3.
+        string.append(
+            r"/mnt/c/Users/LDS/Google 雲端硬碟/code_pool/python/module/utility")
+        symbol.append("/")
+        want.append(9)
+
+        for i in range(len(string)):
+            result = utility.get_symbol_number_in_string(string[i], symbol[i])
+            self.assertEqual(want[i], result)
+
+    def test_remove_last_slash(self):
+        string = []
+        want = []
+
+        # 1.
+        string.append("1/2/3/")
+        want.append("1/2/3")
+        # 2.
+        string.append("1/2/3")
+        want.append("1/2/3")
+        # 3.
+        string.append("1/2/3////////////")
+        want.append("1/2/3")
+        # 3.
+        string.append("//////////")
+        want.append("")
+        # 4.
+        string.append("/8/8/8/8//5//5///")
+        want.append("/8/8/8/8//5//5")
+
+        for i in range(len(string)):
+            result = utility.remove_last_slash(string[i])
+            self.assertEqual(want[i], result)
+
+    def test_get_file_name_list(self):
+        folder_path = []
+        level_limitation = []
+        want = []
+
+        # 1.
+        folder_path.append("test")
+        level_limitation.append(5)
+        want.append(['test/1/11.txt', 'test/2/22.txt'])
+        # 2.
+        folder_path.append("test/")
+        level_limitation.append(5)
+        want.append(['test/1/11.txt', 'test/2/22.txt'])
+
+        for i in range(len(folder_path)):
+            result = utility.get_file_name_list(
+                folder_path[i], level_limitation[i])
+            self.assertEqual(want[i], result)
+
+    # @unittest.skip("")
+    def test_get_file_path_list(self):
+        folder_path = []
+        level_limitation = []
+        want = []
+
+        # 1.
+        folder_path.append("test")
+        level_limitation.append(5)
+        want.append(['test', 'test/1', 'test/2'])
+        # 2.
+        folder_path.append("test/")
+        level_limitation.append(5)
+        want.append(['test', 'test/1', 'test/2'])
+
+        for i in range(len(folder_path)):
+            result = utility.get_file_path_list(
+                folder_path[i], level_limitation[i])
+            self.assertEqual(want[i], result)
+            
+
+    def test_get_file_and_direct_path(self):
+        folder_path = []
+        level_limitation = []
+        want_file = []
+        want_direct = []
+
+        # 1.
+        folder_path.append("test")
+        level_limitation.append(5)
+        want_file.append(['test/1/11.txt', 'test/2/22.txt'])
+        want_direct.append(['test', 'test/1', 'test/2'])
+        # 2.
+        folder_path.append("test/")
+        level_limitation.append(5)
+        want_file.append(['test/1/11.txt', 'test/2/22.txt'])
+        want_direct.append(['test', 'test/1', 'test/2'])
+
+        for i in range(len(folder_path)):
+            file,direct = utility.get_file_and_direct_path(
+                folder_path[i], level_limitation[i])
+            self.assertEqual(want_file[i], file)
+            self.assertEqual(want_direct[i], direct)
+
 
 if __name__ == '__main__':
     unittest.main()
--- utility.py
+++ utility.py
@@ -1,5 +1,8 @@
 
 import json
+import os
+import time
+from typing import final
 
 
 def replace_string(data_list, target, replaced):
@@ -41,3 +44,98 @@ def save_as_log(export_file_path, data):
     with open(export_file_path, mode='w') as file:
         for i in data:
             file.write(i+"\n")
+
+
+def get_symbol_number_in_string(string,symbol):
+    if len(symbol)!=1:
+        return "len(symbol) must be 1"
+    numbers = 0
+    for i in string:
+        if i == "/" or i == "\ ":
+            numbers += 1
+    return numbers
+
+
+def run_single_search(folder_path):
+    file_path=[]
+    direct_path=[]
+    try:
+        for file_name in os.listdir(folder_path):
+            if os.path.isdir(folder_path+'/'+file_name):
+                direct_path.append(folder_path+'/'+file_name)
+            else:
+                file_path.append(folder_path+'/'+file_name)
+    except:
+        print("The is no file in the {}".format(folder_path))
+    finally:
+        return file_path,direct_path
+
+
+def remove_last_slash(string):
+    while True:
+        if len(string)==0: 
+            break
+        elif string[-1]=="/":
+            string=string[:len(string)-1]
+        else:
+            break
+    return string
+
+
+def get_file_name_list(folder_path, level_limitation) -> list:
+    folder_path=remove_last_slash(folder_path)
+    initial_slash_number=get_symbol_number_in_string(folder_path,"/")
+    file_list=[]
+    direct_list=[folder_path]
+    counter = 0
+    while True:
+        level = get_symbol_number_in_string(direct_list[counter],"/")-initial_slash_number
+        if level > level_limitation:
+            break
+        f,d=run_single_search(direct_list[counter])
+        file_list+=f
+        direct_list+=d
+        if counter >= len(direct_list)-1:
+            break
+        counter += 1
+
+    return file_list
+
+
+def get_file_path_list(folder_path, level_limitation) -> list:
+    folder_path=remove_last_slash(folder_path)
+    initial_slash_number=get_symbol_number_in_string(folder_path,"/")
+    file_list=[]
+    direct_list=[folder_path]
+    counter = 0
+    while True:
+        level = get_symbol_number_in_string(direct_list[counter],"/")-initial_slash_number
+        if level > level_limitation:
+            break
+        f,d=run_single_search(direct_list[counter])
+        direct_list+=d
+        if counter >= len(direct_list)-1:
+            break
+        counter += 1
+
+    return direct_list
+
+
+def get_file_and_direct_path(folder_path, level_limitation) -> list:
+    folder_path=remove_last_slash(folder_path)
+    initial_slash_number=get_symbol_number_in_string(folder_path,"/")
+    file_list=[]
+    direct_list=[folder_path]
+    counter = 0
+    while True:
+        level = get_symbol_number_in_string(direct_list[counter],"/")-initial_slash_number
+        if level > level_limitation:
+            break
+        f,d=run_single_search(direct_list[counter])
+        file_list+=f
+        direct_list+=d
+        if counter >= len(direct_list)-1:
+            break
+        counter += 1
+
+    return file_list,direct_list
