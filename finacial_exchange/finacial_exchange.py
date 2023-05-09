81e0f0e568d18a6399b24e50f398f724358edcab
Kevin Li <likevintw@gmail.com>
2023-05-10 01:21:21 +0800

    fail

--- finacial_exchange/finacial_exchange.py
+++ finacial_exchange/finacial_exchange.py
@@ -1,50 +1,58 @@
-
-
 import sys
-sys.path.append("..")
-import utility
+
 
 if __name__ == '__main__':
+    sys.path.append("..")
+    import utility
     input = utility.import_file('input.txt')
     result = []
+    flag = 0
+    text = None
+    number = 0
+    t, n = 0, 0
+    lenth = len(input)
     buffer = []
     amount = 0
-    number = 0
-    text = None
-    for i in range(len(input)):
-        row_data = input[i]
+
+    for row_data in input:
         text = row_data.split()[0]
         number = float(row_data.split()[1])
-
-        if amount == 0:
-            words = "{} {} {}".format(text, number, amount)
-            result.append(words)
-            amount = amount+number
+        print(amount)
+        if number > 0:
+            buffer.append([text, number])
             continue
 
-        if number < 1:
-            buffer.append([text, round(number, 2)])
-            continue
-
-        if number+amount > 0:
-            buffer.append([text, round(number, 2)])
-        else:
-            words = "{} {:.2f} {:.2f}".format(text, -amount, amount)
-            result.append(words)
-            buffer.append([text, round(number-amount, 2)])
-
-            while len(buffer) > 0:
-                t, n = buffer.pop(0)
-                words = "{} {:.2f} {:.2f}".format(text, n, amount)
+        if amount > 0:
+            if amount+number >= 0:
+                # print('{} {:.2f} {:.2f}'.format(text, -amount, 0))
+                result.append('{} {:.2f} {:.2f}'.format(text, number, 0))
+                amount = amount+number
+                continue
+            else:
+                result.append('{} {:.2f} {:.2f}'.format(text, -amount, 0))
+                amount = amount+number
+        while len(buffer) > 0:
+            t, n = buffer.pop(0)
+            if n+number >= 0:
+                # print('{} {:.2f} {:.2f}'.format(t, n, n))
+                # print('{} {:.2f} {:.2f}'.format(
+                #     text, amount, amount+n))
+                result.append('{} {:.2f} {:.2f}'.format(t, n, n))
+                result.append('{} {:.2f} {:.2f}'.format(
+                    text, amount, amount+n))
                 amount = amount+n
-                t, n = buffer.pop(0)
-                words = "{} {:.2f} {:.2f}".format(text, n, amount)
-                amount = amount+n
+                break
+            else:
+                # print('{} {:.2f} {:.2f}'.format(t, n, n))
+                # print('{} {:.2f} {:.2f}'.format(text, -n, 0))
+                result.append('{} {:.2f} {:.2f}'.format(t, n, n))
+                result.append('{} {:.2f} {:.2f}'.format(text, -n, 0))
+                amount += n+number
 
     while len(buffer) > 0:
         t, n = buffer.pop(0)
-        result.append(t+" "+str(round(n, 2)))
-        amount = amount+n
+        # print('{} {:.2f}'.format(t, n, n))
+        result.append('{} {:.2f}'.format(t, n, n))
 
     result.append('amount '+str(round(amount, 2)))
     utility.export_file('result.txt', result)
